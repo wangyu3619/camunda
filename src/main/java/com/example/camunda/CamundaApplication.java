ce86c679f5f52aa9bb96acf50b8c71dc794ecefa
wangyu <wangyu_eva@163.com>
2019-01-04 23:57:33 +0800

    init commit

---
+++ src/main/java/com/example/camunda/CamundaApplication.java
@@ -0,0 +1,17 @@
+package com.example.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangyu
+ */
+@SpringBootApplication
+public class CamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaApplication.class, args);
+    }
+
+}
+
